209c8cf01129baa10523e8500ba43ea75744173a
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-12 22:34:55 -0700

    DeprecationWarnings fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User')
 // @route  POST api/users
 // @desc   register user
 // @access Public
@@ -15,12 +16,33 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const {name, email, password } = req.body;
+    
+    try {
+      // See if user exists
+      let user = await User.findOne({ email});
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists '}] });
+      }
+      // Get users gravatar
+  
+      // Encrypt password
+  
+      // Return jsonwebtoken
+  
+      res.send('User route');
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+
   }
 );
 
