5343b909f028b822790b1dfb3a2c0660c7a868a1
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-13 23:04:49 -0700

    Delete profile & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -131,4 +131,23 @@ router.get('/user/:user_id', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route  Delete api/profile
+// @desc   Delete profile, user & posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //@todo - remove users posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Romove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
 module.exports = router;
