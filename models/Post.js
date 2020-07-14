66d5fba9dde0ef027c748a8e1673aad70627444d
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-14 11:27:49 -0700

    Add Post Route

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongooese');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
@@ -52,4 +52,4 @@ const PostSchema = new Schema({
   }
 });
 
-module.exports = Post = mongoose.medel('post');
+module.exports = Post = mongoose.model('post', PostSchema);
