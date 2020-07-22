4568f46ba7ba02a90d99af65428dc0b2992d565f
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-21 21:53:09 -0700

    Post Item Component

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,6 +6,7 @@ import { GET_POSTS, POST_ERROR } from './types';
 export const getPosts = () => async (dispatch) => {
   try {
     const res = await axios.get('/api/posts');
+
     dispatch({
       type: GET_POSTS,
       payload: res.data
