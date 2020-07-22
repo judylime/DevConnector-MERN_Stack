be67457fb0d2ea27a41d2d61194648599f45bb26
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-22 16:05:44 -0700

    cd client => npm run build=> build folder => index.html

---
+++ client/build/precache-manifest.7671453c29d4cac901dc35354a630dbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb74a6e14ecc952a155dae0de9a2d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9af9295d95fe1d34ba5b",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "046902d0097652bd3ae7",
+    "url": "/static/js/2.6a79458a.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.6a79458a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af9295d95fe1d34ba5b",
+    "url": "/static/js/main.32554756.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
