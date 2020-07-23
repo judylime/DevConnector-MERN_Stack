20aec9715ea25d5aea0cb115d17cf3b59db941fa
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-22 23:31:25 -0700

    Not Found Page& Theme workaround

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react';
 
-export const NotFound = () => {
+const NotFound = () => {
   return (
-    <div>
       <Fragment>
         <h1 className='x-large text-primary'>
-          <i className='fas fa-exclamation-triangle'></i> Page Not Found
+          <i className='fas fa-exclamation-triangle' /> Page Not Found
         </h1>
-        <p className='large'>Sorry , this page does not exist</p>
+        <p className='large'>Sorry, this page does not exist</p>
       </Fragment>
-    </div>
   );
 };
+
+export default NotFound;
